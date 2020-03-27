91943a8f3e174ca9d77e6eb4eeab335d3fb4fb39
Danny <goody49172@Gmail.com>
2020-03-27 00:24:10 -0700

    started basis

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+
+function logo()
+{
+    document.getElementById("logo").innerHTML ="Learning Right";
+    document.getElementById("logo").style.backgroundColor ="lightgreen";
+    document.getElementById("logo").style.textAlign="center";
+}
